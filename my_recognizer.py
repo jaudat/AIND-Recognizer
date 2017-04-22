b64b7cb523d403af42915c95617bc0de30313715
Jaudat Syed <sjh.jaudat@gmail.com>
2017-04-22 02:09:46 -0400

    Finished Part 3

--- my_recognizer.py
+++ my_recognizer.py
@@ -20,6 +20,33 @@ def recognize(models: dict, test_set: SinglesData):
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
-    # TODO implement the recognizer
+
+    # https://discussions.udacity.com/t/recognizer-implementation/234793/5
+
+    test_sequences = list(test_set.get_all_Xlengths().values())
+    for test_X, test_Xlength in  test_sequences:
+        probability = {}
+        best_word = None
+        best_logL = float("-inf")
+        for word, model in models.items():
+            try:
+                loop_logL = model.score(test_X, test_Xlength)
+                probability[word] = loop_logL
+                if loop_logL > best_logL:
+                    best_word = word
+                    best_logL = loop_logL
+            except:
+                probability[word] = float('-inf')
+
+        guesses.append(best_word)
+        probabilities.append(probability)
+
     # return probabilities, guesses
-    raise NotImplementedError
+    return probabilities, guesses
+
+# if __name__ == "__main__":
+#     from asl_test_recognizer import TestRecognize
+#     test_model = TestRecognize()
+#     test_model.setUp()
+#     test_model.test_recognize_guesses_interface()
+#     test_model.test_recognize_guesses_interface()
